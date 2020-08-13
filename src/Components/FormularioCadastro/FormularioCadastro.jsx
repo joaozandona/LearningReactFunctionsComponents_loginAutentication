7237e4f4546c2230f933b7be7d9739164b2d7926
Joao Zandona <joaoricardozandona@hotmail.com>
2020-08-13 18:09:16 -0300

    Esta salvando o nome e o sobrenome em uma variavem. Terminando a parte tres

--- src/Components/FormularioCadastro/FormularioCadastro.jsx
+++ src/Components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,44 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
 
 function FormularioCadastro(){
-
+    const [nome, setNome] = useState("");
+    const [sobrenome, setSobrenome] = useState("");
+    
     return(
-        <form>
-            <TextField
-                id="nome" 
-                label="Nome" 
-                variant="outlined" 
-                margin="normal"
-                fullWidth 
-            />
-            <TextField 
-                id="sobrenome" 
-                label="Sobrenome" 
-                variant="outlined" 
-                margin="normal" 
-                fullWidth 
-            />
-            <TextField 
-                id="CPF" 
-                label="CPF" 
-                variant="outlined" 
-                fullWidth 
-                margin="normal" 
-            />
+    <form
+        onSubmit={(event) => {
+        event.preventDefault();
+        console.log(nome, sobrenome);}}>
 
-            <FormControlLabel 
-                label="Promoções" 
-                control={<Switch name="promocoes" defaultChecked={true} color="primary"/>}
-            />
-            <FormControlLabel 
-                label="Novidades" 
-                control={<Switch name="novidades" defaultChecked={true} color="primary" />}
-            />
+        <TextField
+                value={nome}
+                
+                onChange={(event) => {
+                    let tmpNome = event.target.value;
+                    if(tmpNome.length >= 3){
+                        tmpNome = tmpNome.substr(0, 3);
+                    }
+                    setNome(tmpNome);
+                    
+                }}
+            id="nome" 
+            label="Nome" 
+            variant="outlined" 
+            margin="normal"
+            fullWidth 
+        />
+        <TextField
+            value={sobrenome}
+            onChange={(event) => {
+                setSobrenome(event.target.value);
+            }} 
+            id="sobrenome" 
+            label="Sobrenome" 
+            variant="outlined" 
+            margin="normal" 
+            fullWidth 
+        />
+        <TextField 
+            id="CPF" 
+            label="CPF" 
+            variant="outlined" 
+            fullWidth 
+            margin="normal" 
+        />
 
-            <Button variant="contained" color="primary" type="sumbit">Cadastrar</Button>
-        </form>
-    );
+        <FormControlLabel 
+            label="Promoções" 
+            control={<Switch name="promocoes" defaultChecked={true} color="primary"/>}
+        />
+        <FormControlLabel 
+            label="Novidades" 
+            control={<Switch name="novidades" defaultChecked={true} color="primary" />}
+        />
+
+        <Button variant="contained" color="primary" type="sumbit">Cadastrar</Button>
+    </form>
+);
 
 }
 
